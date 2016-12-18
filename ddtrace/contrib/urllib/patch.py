82c60fddb9781b5b34f5dcf47fc6d0a8c8eff1f8
brettlangdon <brett@blangdon.com>
2016-12-18 12:54:42 -0500

    Add urllib/urllib2/urllib.request patching

---
+++ ddtrace/contrib/urllib/patch.py
@@ -0,0 +1,82 @@
+# Standard library
+import logging
+
+# Third party
+import wrapt
+
+# Project
+from ... import tracer
+from ...compat import PY2, string_type
+from ...ext import http
+
+log = logging.getLogger(__name__)
+
+
+def patch():
+    wrap_modules = []
+    if PY2:
+        wrap_modules = [
+            ('urllib', 'urlopen'),
+            ('urllib2', 'urlopen'),
+        ]
+    else:
+        wrap_modules = [
+            ('urllib.request', 'urlopen'),
+        ]
+
+    for module, func in wrap_modules:
+        trace_name = '{module}.{func}'.format(module=module, func=func)
+        wrapt.wrap_function_wrapper(module, func, _get_urlopen_wrapper(trace_name))
+
+
+def _get_urlopen_wrapper(trace_name):
+    def _wrapped_urlopen(func, instance, args, kwargs):
+        if not tracer.enabled:
+            return func(*args, **kwargs)
+
+        with tracer.trace(trace_name, span_type=http.TYPE) as span:
+            try:
+                _apply_request_tags(span, args, kwargs)
+            except Exception:
+                log.warn('error applying request tags', exc_info=True)
+
+            resp = None
+            try:
+                resp = func(*args, **kwargs)
+                return resp
+            finally:
+                try:
+                    _apply_response_tags(span, resp)
+                except Exception:
+                    log.warn('error applying response tags', exc_info=True)
+
+    return _wrapped_urlopen
+
+
+def _apply_request_tags(span, args, kwargs):
+    url = kwargs.get('url') or args[0]
+    data = kwargs.get('data')
+    if not data and len(args) > 1:
+        data = args[1]
+
+    if isinstance(url, string_type):
+        span.set_tag(http.URL, url)
+        method = 'GET' if not data else 'POST'
+        span.set_tag(http.METHOD, method)
+    else:
+        span.set_tag(http.URL, url.get_full_url())
+        span.set_tag(http.METHOD, url.get_method())
+
+
+def _apply_response_tags(span, resp):
+    if not resp:
+        return
+
+    status_code = resp.getcode()
+    span.set_tag(http.STATUS_CODE, status_code)
+    span.error = int(500 <= status_code)
+
+    if 'content-length' in resp.headers:
+        span.set_tag('http.response.length', int(resp.headers['content-length']))
+    if 'content-type' in resp.headers:
+        span.set_tag('http.response.type', int(resp.headers['content-type']))
